5ecd1a8aa8598ed3b57af643602c0f6da82aa477
MySticHorYzoN <fastor26@gmail.com>
2018-08-07 20:07:35 +0200

    Add files via upload

---
+++ src/CustomJoinNQuit/Main.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace CustomJoinNQuit;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\Server;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\event\Event;
+use pocketmine\Player;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+
+class Main extends PluginBase implements Listener{
+
+public function onEnable(){
+
+$this->getServer()->getPluginManager()->registerEvents($this, $this);
+$this->getServer()->getLogger()->info("§aJoin§fN§cQuit §aActivated");
+
+}
+
+public function onJoin(PlayerJoinEvent $event){
+
+$player = $event->getPlayer();
+$name = $player->getName();
+$event->setJoinMessage("");
+$this->getServer()->broadcastMessage("§0[§b*§0] §a" . $name);
+
+}
+
+public function onQuit(PlayerQuitEvent $event){
+
+$player = $event->getPlayer();
+$name = $player->getName();
+$event->setQuitMessage("");
+$this->getServer()->broadcastMessage("§0[§c*§0] §c" . $name);
+
+}
+
+}
